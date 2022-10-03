7a5d718a0484f53baf93f730f620b07f85d1d087
Yousuf Islam Rathore <rathoreislam@gmail.com>
2022-10-03 14:55:28 +0500

    First Commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+function hoverDiv1(div, key) {
+    if(key == 0) {
+        div.firstElementChild.className += " py-6" 
+    } else {
+        div.firstElementChild.className += "fail"
+    }
+
+    // console.log(true)
+}
